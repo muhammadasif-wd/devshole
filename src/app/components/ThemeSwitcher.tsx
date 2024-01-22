33baa442acbf677e0d88f72347a0fa4a0eabcaeb
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-01-22 16:36:22 +0600

    [FEAT] - add nextui and theme setup dark, light - [DONE]

---
+++ src/app/components/ThemeSwitcher.tsx
@@ -0,0 +1,41 @@
+"use client";
+import {Switch} from "@nextui-org/react";
+import {useTheme} from "next-themes";
+import {useEffect, useState} from "react";
+
+import {SunIcon} from "@/assets/icons/sun-icon";
+import {MoonIcon} from "@/assets/icons/moon-icon";
+
+export function ThemeSwitcher(): React.ReactNode {
+  const [mounted, setMounted] = useState(false);
+  const {theme, setTheme} = useTheme();
+
+  useEffect(() => {
+    if (!theme) {
+      setTheme("dark");
+    }
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const handleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  return (
+    <Switch
+      color="secondary"
+      defaultSelected={theme === "light"}
+      size="lg"
+      thumbIcon={({isSelected, className}) =>
+        isSelected ? <SunIcon className={className} /> : <MoonIcon className={className} />
+      }
+      onClick={handleTheme}
+    />
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import {ThemeSwitcher} from "./components/ThemeSwitcher";
+
 export default function Home() {
   return (
     <main className="">
-      <h1>Hello</h1>
+      <h1 className="">Hello</h1>
+      <ThemeSwitcher />
     </main>
   );
 }
